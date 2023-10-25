7529bc11e154ab762d4afcdb904616d4259c6ab5
Prila1 <yarteypriscy@gmail.com>
2023-10-25 21:55:30 +0000

    sort.h

--- sort.h
+++ sort.h
@@ -2,8 +2,6 @@
 #define SORT_H
 
 #include <stdlib.h>
-#include <stdio.h>
-
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -33,4 +31,5 @@ void heap_sort(int *array, size_t size);
 void radix_sort(int *array, size_t size);
 void bitonic_sort(int *array, size_t size);
 void quick_sort_hoare(int *array, size_t size);
+
 #endif
